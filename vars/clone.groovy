d5d351522eed0f7fd3cb4ec533c39d1eb9edaa5f
Abhishek <108752818+heynexus007@users.noreply.github.com>
2025-12-05 14:00:03 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -2,5 +2,5 @@ def call(String url, String branch){
   echo "Cloning the git url"
   git url: "${url}", branch: "${branch}"
   echo "Cloning is Successful!"
-  sh "cat QuickSort.java"
+  //sh "cat QuickSort.java"
 }
